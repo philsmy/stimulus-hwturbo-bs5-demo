131ad85d78322ac166e786114ff720d067450b0b
Phil Smy <phil@philsmy.com>
2021-08-28 22:00:54 +0900

    test tweaks

--- spec/models/time_tracker_spec.rb
+++ spec/models/time_tracker_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe TimeTracker, type: :model do
   it 'should not have a status' do
     naked_time_tracker = TimeTracker.new
-    expect(naked_time_tracker).to be_valid
+    expect(naked_time_tracker).to_not be_valid
   end
 end
